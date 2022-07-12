3b65325a0166931e64cd445f039fb062b30b0a15
jvsmscript <dbase913@gmail.com>
2022-07-12 20:23:23 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return(
+  <>
     <h1>ola mundo</h1>
+    <NotificationButton />
+    <NotificationButton />
+  </>
   )
 }
 
