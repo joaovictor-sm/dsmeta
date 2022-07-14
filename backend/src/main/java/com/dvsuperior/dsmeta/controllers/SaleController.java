f4761ab188fced86e4f849aa114f547784f1f7b0
jvsmscript <dbase913@gmail.com>
2022-07-14 14:21:33 -0300

    API test

---
+++ backend/src/main/java/com/dvsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.dvsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dvsuperior.dsmeta.entities.Sale;
+import com.dvsuperior.dsmeta.services.SalesService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SalesService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
