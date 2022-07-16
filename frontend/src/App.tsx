a3054ab194b1237bfcea6eb229f66e623cbff0b6
jvsmscript <dbase913@gmail.com>
2022-07-15 22:55:38 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./assets/components/Header";
 import NotificationButton from "./assets/components/NotificationButton";
 import SalesCard from "./assets/components/SalesCard";
 
+
 function App() {
   return(
   <>
+  <ToastContainer />
     <Header />
     <main>
       <section id="sales">
