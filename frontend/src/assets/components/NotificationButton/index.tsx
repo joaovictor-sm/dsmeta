2e9276b337c73f424a399529c5836231a794a6bd
jvsmscript <dbase913@gmail.com>
2022-07-15 22:45:44 -0300

    Send notification

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,14 +1,27 @@
+import axios from 'axios';
 import icon from '../../img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+
 import './style.css';
 
-function NotificationButton() {
-    return(
-      <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
-      </div>
-     
-    )
-  }
-  
-  export default NotificationButton;
-  
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(Response => {
+      console.log("SUCESSO");
+    });
+}
+
+function NotificationButton({ saleId }: Props) {
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="Notificar" />
+    </div>
+
+  )
+}
+
+export default NotificationButton;
